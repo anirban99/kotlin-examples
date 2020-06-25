587e09942f8f40799fcc61fc45ac006851c36fff
Anirban Chatterjee <canirban99@gmail.com>
2020-06-25 23:37:10 +0200

    Initial Project Add

---
+++ src/main/kotlin/com/example/tutorials/KotlinTutorialsApplication.kt
@@ -0,0 +1,11 @@
+package com.example.tutorials
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinTutorialsApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinTutorialsApplication>(*args)
+}
