9e0a0677a15ac35a8906768efca6df4661062940
Anirban Chatterjee <canirban99@gmail.com>
2020-06-29 00:36:05 +0200

    Added operators and tests

--- src/main/kotlin/com/example/tutorials/nullsafety/NullSafety.kt
+++ src/main/kotlin/com/example/tutorials/nullsafety/NullSafety.kt
@@ -42,39 +42,71 @@ class NullSafety {
         return country?.city?.code
     }
 
-    fun safeCallWithLet(){
-
+    fun safeCallWithLet(): List<String?> {
+        val cities: List<String?> = listOf("Kolkata", null, "Mumbai")
+        var name: List<String?> = emptyList()
+        for (city in cities) {
+            city?.let { name = name.plus(it) }
+        }
+        return name
     }
 
-    fun safeCallWithAlso(){
-
+    fun safeCallWithAlso(): List<String?> {
+        val cities: List<String?> = listOf("Kolkata", null, "Mumbai")
+        var name: List<String?> = emptyList()
+        for (city in cities) {
+            city?.let {
+                name = name.plus(it)
+                it
+            }?.also { println("Logging the value: $it") }
+        }
+        return name
     }
 
-    fun safeCallWithRun(){
-
+    fun safeCallWithRun(): List<String?> {
+        val countries: List<String?> = listOf("India", null, "Germany")
+        var name: List<String?> = emptyList()
+        for (country in countries) {
+            country?.run {
+                name = name.plus(this)
+                this
+            }?.also { println("Logging the value: $it") }
+        }
+        return name
     }
 
-    fun elvisOperatorForValue(){
-
+    fun elvisOperatorForDefaultValue(): String {
+        val country: Country? = Country(City("New Delhi", null))
+        return country?.city?.code ?: "Not available"
     }
 
-    fun elvisOperator(){
-
+    fun elvisOperator(): String {
+        val country: Country? = Country(City("Mumbai", "002"))
+        return country?.city?.code ?: "Not available"
     }
 
-    fun elvisOperatorForException(){
-
+    fun elvisOperatorForException(): String {
+        val country: Country? = Country(City("Chennai", null))
+        return country?.city?.code ?: throw IllegalArgumentException("Not a valid code")
     }
 
-    fun notNullAssertion(){
-
+    fun notNullAssertion(): Int {
+        val country: String? = "India"
+        return country!!.length
     }
 
-    fun filterList(){
+    fun notNullAssertionForException(): Int {
+        val country: String? = null
+        return country!!.length
+    }
 
+    fun filterNotNullList(): List<String> {
+        val countries: List<String?> = listOf("India", null, "Germany", "Russia", null)
+        val filteredList: List<String> = countries.filterNotNull()
+        return filteredList
     }
 
     data class Country(val city: City?)
 
-    data class City(val name: String?, val code: String?)
+    data class City(val name: String, val code: String?)
 }
--- src/test/kotlin/com/example/tutorials/NullSafetyUnitTest.kt
+++ src/test/kotlin/com/example/tutorials/NullSafetyUnitTest.kt
@@ -4,6 +4,7 @@ import com.example.tutorials.nullsafety.NullSafety
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class NullSafetyUnitTest {
 
@@ -38,5 +39,53 @@ class NullSafetyUnitTest {
     fun givenNullableString_whenSafeCallChainIsDone_thenReturnsNull() {
         assertEquals("003", classUnderTest.safeCallChainForValue())
         assertNull(classUnderTest.safeCallChain())
+    }
+
+    @Test
+    fun givenList_whenSafeCallWithLet_thenReturnsOnlyNonNullableElements() {
+        assertEquals(2, classUnderTest.safeCallWithLet().size)
+        assertEquals("Kolkata", classUnderTest.safeCallWithLet()[0])
+    }
+
+    @Test
+    fun givenList_whenSafeCallWithLetAndAlso_thenReturnsOnlyNonNullableElements() {
+        assertEquals(2, classUnderTest.safeCallWithAlso().size)
+    }
+
+    @Test
+    fun givenList_whenSafeCallWitRun_thenReturnsOnlyNonNullableElements() {
+        assertEquals(2, classUnderTest.safeCallWithRun().size)
+        assertEquals("Germany", classUnderTest.safeCallWithRun()[1])
+    }
+
+    @Test
+    fun givenNullableString_whenElvisOperatorIsUsed_thenReturnsDefaultValue() {
+        assertEquals("Not available", classUnderTest.elvisOperatorForDefaultValue())
+    }
+
+    @Test
+    fun givenNonNullableString_whenElvisOperatorIsUsed_thenReturnsValue() {
+        assertEquals("002", classUnderTest.elvisOperator())
+    }
+
+    @Test
+    fun givenNullableString_whenElvisOperatorIsUsed_thenThrowsException() {
+        assertThrows<IllegalArgumentException> { classUnderTest.elvisOperatorForException() }
+    }
+
+    @Test
+    fun givenNonNullableString_whenNotNullAssertionOperatorIsUsed_thenReturnsValue() {
+        assertEquals(5, classUnderTest.notNullAssertion())
+    }
+
+    @Test
+    fun givenNullableString_whenNotNullAssertionOperatorIsUsed_thenThrowsException() {
+        assertThrows<NullPointerException> { classUnderTest.notNullAssertionForException() }
+    }
+
+    @Test
+    fun givenList_whenFilterNotNullIsUsed_thenReturnsOnlyNonNullableElements() {
+        assertEquals(3, classUnderTest.filterNotNullList().size)
+        assertEquals("Russia", classUnderTest.filterNotNullList()[2])
     }
 }
